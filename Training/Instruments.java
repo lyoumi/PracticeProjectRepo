4134af988b8a28e7d11d36609186a01e6e61aa8e
Artjom <newframework@protonmail.com>
2017-07-05 15:58:00 +0300

    Update Instruments.java

--- Training/Instruments.java
+++ Training/Instruments.java
@@ -15,19 +15,32 @@ public class Instruments {
         String s = scanner.nextLine();
         switch (s){
             case "Percussion": {
-                System.out.println("Pls, enter song....");
+                System.out.println("Pls, enter sing....");
                 String song = scanner.nextLine();
                 instrument = new Percussion(song);
                 sing();
                 break;
             }
             case "Wind": {
-                System.out.println("Pls, enter sing....");
+                System.out.println("Pls, enter song....");
                 String song = scanner.nextLine();
-                instrument = new Wind(song);
+                System.out.println("Pls, enter kind of wind instruments....");
+                String kind = scanner.nextLine();
+                switch (kind){
+                    case "Brass":{
+                        instrument = new Brass(song);
+                        break;
+                    }
+                    case "WoodWind":{
+                        instrument = new WoodWind(song);
+                        break;
+                    }
+                    default: instrument = new Wind(song);
+                }
                 sing();
                 break;
             }
+            default: break;
         }
     }
 
@@ -67,7 +80,7 @@ class Percussion extends Instrument {
 
     @Override
     String song() {
-        return song;
+        return "Playing song: " + song + "....";
     }
 }
 
@@ -81,18 +94,60 @@ class Wind extends Instrument {
 
     @Override
     void play() {
-        System.out.println("Play the trumpet!");
+        System.out.println("Play the unknown wind instrument....!");
     }
 
     @Override
     void adjust() {
-        System.out.println("Trumpet breeze!");
+        System.out.println("Adjust unknown wind instrument....");
     }
 
     @Override
     String song() {
-        return song;
+        return "Playing song: " + song + "....";
     }
 }
 
+class WoodWind extends Wind{
 
+    WoodWind(String song) {
+        super(song);
+    }
+
+    @Override
+    void play() {
+        System.out.println("You play WoodWind!");
+    }
+
+    @Override
+    String song() {
+        return super.song();
+    }
+
+    @Override
+    void adjust() {
+        System.out.println("Adjust WoodWind!");
+    }
+}
+
+class Brass extends Wind{
+
+    Brass(String song) {
+        super(song);
+    }
+
+    @Override
+    void play() {
+        System.out.println("You play BrassWind!");
+    }
+
+    @Override
+    String song() {
+        return super.song();
+    }
+
+    @Override
+    void adjust() {
+        System.out.println("Adjust BrassWind!");
+    }
+}
