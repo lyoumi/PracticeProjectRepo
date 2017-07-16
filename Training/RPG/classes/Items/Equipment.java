31156c1021a4c531aea1ef02c4b36fdecf86df89
Artjom <newframework@protonmail.com>
2017-07-16 12:34:26 +0300

    Add files via upload

---
+++ Training/RPG/classes/Items/Equipment.java
@@ -0,0 +1,9 @@
+package Training.RPG.classes.Items;
+
+/**
+ * Created by pikachu on 14.07.17.
+ */
+public interface Equipment {
+    void equip(Items item);
+    void unEquip();
+}
--- Training/RPG/classes/Items/UsingItems.java
+++ Training/RPG/classes/Items/UsingItems.java
@@ -7,7 +7,6 @@ import Training.RPG.classes.Monsters.Monster;
  * Created by pikachu on 14.07.17.
  */
 public interface UsingItems {
-    void add(Items item);
+    boolean add(Items item);
     void use(Items item);
-    void drop(Human human, Monster monster);
 }
