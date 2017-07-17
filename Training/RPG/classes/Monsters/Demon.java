6dffd6143d5f78d088dbed79d279761afd7c109f
Artjom <newframework@protonmail.com>
2017-07-17 19:14:14 +0300

    Update Demon.java

--- Training/RPG/classes/Monsters/Demon.java
+++ Training/RPG/classes/Monsters/Demon.java
@@ -1,6 +1,11 @@
-package Training.RPG.classes.Monsters;
+package RPG.classes.Monsters;
 
-import Training.RPG.classes.Items.Items;
+import RPG.classes.Items.Items;
+import RPG.classes.Items.items.Item;
+import RPG.classes.Items.items.armors.armors.IronChest;
+import RPG.classes.Items.items.armors.boots.IronBoots;
+import RPG.classes.Items.items.armors.helmets.IronHelmet;
+import RPG.classes.Items.items.weapons.weapons.Sword;
 
 import java.util.*;
 
@@ -13,14 +18,17 @@ public class Demon implements Monster {
     private static final int sizeOfItems = itemsList.size();
     private static final Random random = new Random();
 
+    private int HERO_LEVEL;
+
     private int damage;
     private int hitPoint;
     private int experience = 100;
     private LinkedList<Items> inventory = new LinkedList<>();
 
-    public Demon(int level){
-        hitPoint = (level+1)*50;
-        damage = (level+1)*6;
+    public Demon(int HERO_LEVEL){
+        hitPoint = (HERO_LEVEL+1)*50;
+        damage = (HERO_LEVEL+1)*6;
+        this.HERO_LEVEL = HERO_LEVEL;
     }
 
     public int getExperience(){
@@ -51,6 +59,26 @@ public class Demon implements Monster {
     public LinkedList<Items> getInventory() {
         inventory.add(itemsList.get(random.nextInt(sizeOfItems)));
         return inventory;
+    }
+
+    public Item getDroppedItems(){
+        switch (random.nextInt(4)){
+            case 0:{
+                return new Sword(HERO_LEVEL);
+            }
+            case 1: {
+                return new IronChest(HERO_LEVEL);
+            }
+            case 2: {
+                return new IronHelmet(HERO_LEVEL);
+            }
+            case 3: {
+                return new IronBoots(HERO_LEVEL);
+            }
+            default: {
+                return null;
+            }
+        }
     }
 
     public String toString(){
